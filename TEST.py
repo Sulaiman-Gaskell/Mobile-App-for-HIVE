fa2d910e4e0e5854bf9a915c1b78c623ef7ff85d
Sulaiman-Gaskell <103843022+Sulaiman-Gaskell@users.noreply.github.com>
2023-04-12 14:41:39 +0100

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class MainApp(App):
+    def build(self):
+        label = Label(text='Hello from Kivy',
+                      size_hint=(.5, .5),
+                      pos_hint={'center_x': .5, 'center_y': .5})
+
+        return label
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
