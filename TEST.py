6fd89dce75a6d1d3d75f15ad0190fc7e62321c51
Sulaiman-Gaskell <103843022+Sulaiman-Gaskell@users.noreply.github.com>
2023-04-21 19:24:00 +0100

    Add files via upload

--- TEST.py
+++ TEST.py
@@ -4,14 +4,19 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
 
 class MainApp(App):
     def build(self):
-        label = Label(text='Hello there',
+        button = Button(text='Hello from Kivy',
                       size_hint=(.5, .5),
                       pos_hint={'center_x': .5, 'center_y': .5})
+        button.bind(on_press=self.update)
 
-        return label
+        return button
+    
+    def update(self,event):
+        self.button.text = 'Hello to Kivy'
 
 if __name__ == '__main__':
     app = MainApp()
